6dff37b294730224de8d37c21ed4b4d39a3ab0ee
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-06-30 12:32:18 -0700

    Header, Footer, Odometer all working, need to pad

--- src/App.js
+++ src/App.js
@@ -5,22 +5,79 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Header title="Home page" />
+      <hr />
+      <Odometer />
+      <hr />
+      <Footer author="Joseph Zabaleta" />
     </div>
   );
 }
+
+function Header(props) {
+  return (
+  <header>
+    <h1>{props.title}</h1>
+  </header>
+  )
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <h4>
+        React App by: {props.author}
+      </h4>
+    </footer>
+  )
+}
+
+class Odometer extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      count : 0,
+    }
+  }
+
+  increaseCount(value) {
+
+    let current = this.state.count;
+    let tempState = 0;
+
+    if ((current + value) > 9999) {
+      tempState = ((current + value) - 9999)
+    } else {
+      tempState = (current + value)
+    }
+    
+    this.setState({
+      count : tempState,
+    })
+  }
+
+  // padNum(num) {
+  //   let tempNum = '';
+  //   if (num < 10) {
+  //     tempNum = num.toString().padStart(4, 0)
+  //   }
+  //   return parseInt(tempNum)
+  // }
+
+  render() {
+    return (
+      <>
+      <button onClick={() => this.increaseCount(1)}>Add 1</button>
+      <button onClick={() => this.increaseCount(10)}>Add 10</button>
+      <button onClick={() => this.increaseCount(100)}>Add 100</button>
+      <button onClick={() => this.increaseCount(1000)}>Add 1000</button>
+      <h1>Odometer: {this.state.count}</h1>
+      </>
+    )
+  }
+}
+
 
 export default App;
